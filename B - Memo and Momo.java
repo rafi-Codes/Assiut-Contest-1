9fc6af6ab7fe7d6b05a149e50d5f4e333a5fa591
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-10-02 02:46:43 +0600

    Create B - Memo and Momo.java

---
+++ B - Memo and Momo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class codeforces{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long k = sc.nextLong();
+ 
+        if (a % k == 0 && b % k == 0){
+            System.out.println("Both");
+        } else  if (a % k == 0){
+            System.out.println("Memo");
+        } else  if (b % k == 0){
+            System.out.println("Momo");
+        } else {
+            System.out.println("No One");
+        }
+        sc.close();
+    }
+}
